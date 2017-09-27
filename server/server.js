bbc6727c0cf378216bdbbe79f5fd06e7c28d1367
Nicholas Brink <nbrink7@gmail.com>
2017-09-27 17:41:47 -0500

    Add validation to Todo.

--- server/server.js
+++ server/server.js
@@ -1,42 +1,34 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connection.openUri('mongodb://localhost:27017/TodoApp')
+mongoose.connection.openUri('mongodb://localhost:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
     text: {
         type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 const newTodo = new Todo({
-    text: 'Cook dinner'
+    text: '    Trim test   '
 });
 
 newTodo.save().then(
-    (doc) => {
-        console.log('Saved todo', doc);
-    }, (e) => {
-        console.log('Unable to save!')
-    }
-);
-
-const otherTodo = new Todo({
-    text: 'Wake up in the morning',
-    completed: true,
-    completedAt: 1234
-});
-
-otherTodo.save().then(
-    (doc) => {
-        console.log('Saved todo', doc);
-    }, (e) => {
+    doc => {
+        console.log(JSON.stringify(doc, undefined, 2));
+    },
+    () => {
         console.log('Unable to save!')
     }
 );
