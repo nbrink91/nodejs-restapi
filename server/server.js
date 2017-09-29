e892cd3479e64a995718ad8e89c11c9228e6d471
Nicholas Brink <nbrink7@gmail.com>
2017-09-28 19:49:44 -0500

    Reorganize code a bit.

--- server/server.js
+++ server/server.js
@@ -15,16 +15,14 @@ app.post('/todos', (req, res) => {
     });
 
     todo.save().then(
-        todo => res.send(todo),
-        e => res.status(400).send(e)
-    )
+        todo => res.send(todo)
+    ).catch(e => res.status(400).send(e));
 });
 
 app.get('/todos', (req, res) => {
     Todo.find().then(
         todos => res.send({todos}),
-        () => res.status(400).send(e)
-    );
+    ).catch(e => res.status(400).send(e));
 });
 
 app.get('/todos/:id', (req, res) => {
@@ -33,10 +31,12 @@ app.get('/todos/:id', (req, res) => {
         res.send('ID not valid')
     }
 
-    Todo.findById(id).then(
-        todo => res.send(todo),
-        e => res.status(400).send(e)
-    )
+    Todo.findById(id).then(todo => {
+        if (!todo) {
+            return res.status(404).send(`Unable to find todo with id ${id}`)
+        }
+        res.send({todo});
+    }).catch(e => res.status(400).send(e))
 });
 
 app.listen(3000, () => {
